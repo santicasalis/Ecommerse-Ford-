267614e542b093fa9bc0426a1ebbe33f316f3cb2
santicasalis <santicasalis@hotmail.com>
2022-05-18 22:58:29 +0200

    item

--- src/components/ItemDetail/Detalle.jsx
+++ src/components/ItemDetail/Detalle.jsx
@@ -5,7 +5,7 @@ import { Button} from "react-bootstrap"
 
 
 export const Detalle = ( )   => {
-    const vehiculo = {id:"11", categoria:"autos", producto:"Mustang",precio:"80.000", stock:1, imagen:"https://www.ar.ford.com/manual/img/mustang.jpg", descripcion:"Nuevo Mustang Match V8 5.0L  AT "}
+    const vehiculo = {id:"11", categoria:"autos", producto:"Mustang",precio:"80.000", stock:1, imagen:"https://www.ford.com.ar/content/dam/Ford/website-assets/latam/ar/nameplate/mustang/mustang-2021/colorizer/negro-ebony/far-mustang-negro-ebony-.jpg.dam.full.high.jpg/1622062692550.jpg", descripcion:"Nuevo Mustang Match V8 5.0L  AT "}
   return (
    <div className='col-md-3'>
       
